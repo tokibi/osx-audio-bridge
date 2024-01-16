f4f2d9f2479eefa31beafad9525c07713cf3c5ed
tokibi <tatu014@gmail.com>
2024-01-16 16:51:55 +0900

    Add protobuf schemas

--- Package.swift
+++ Package.swift
@@ -9,10 +9,13 @@ let package = Package(
     products: [
         .executable(name: "OSXAudioBridgeCLI", targets: ["OSXAudioBridgeCLI"]),
         .library(name: "CaptureEngine", targets: ["CaptureEngine"]),
+        .library(name: "CaptureServer", targets: ["CaptureServer"])
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
         .package(url: "https://github.com/kylef/Spectre.git", from: "0.10.0"),
+        .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.21.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.62.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -21,13 +24,23 @@ let package = Package(
             name: "OSXAudioBridgeCLI",
             dependencies: [
                 "CaptureEngine",
+                "CaptureServer",
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ]
         ),
         .target(
             name: "CaptureEngine",
             dependencies: [
-
+            ]
+        ),
+        .target(
+            name: "CaptureServer",
+            dependencies: [
+                "CaptureEngine",
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "GRPC", package: "grpc-swift"),
+                .product(name: "NIO", package: "swift-nio"),
+                // .product(name: "NIOHTTP2", package: "swift-nio-http2"),
             ]
         ),
         .testTarget(
